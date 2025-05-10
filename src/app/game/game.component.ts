ed0501ca1dcc1679b02887c37154988a514dc326
RobbyR98 <robbyrunge@yahoo.com>
2025-05-10 11:30:20 +0200

    implement game logic and card handling in GameComponent

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
@@ -12,8 +13,28 @@ import { Component } from '@angular/core';
 export class GameComponent {
 
   pickCardAnimation = false;
+  currentCard: string | undefined = '';
+  game: Game = new Game();
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
 
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      this.pickCardAnimation = true;
+      setTimeout(() => {
+        this.pickCardAnimation = false;
+      }, 1500);
+    }
   }
- }
+}
