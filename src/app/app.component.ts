7496f23093cd3a75cfa93d5388b151e1a0afa7bb
RobbyR98 <robbyrunge@yahoo.com>
2025-05-15 21:03:55 +0200

    refactor: enhance AppComponent to integrate Firestore and manage item data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [AsyncPipe, CommonModule, RouterOutlet],
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
 })
 
 export class AppComponent {
-  title = 'ringoffire';
+  firestore: Firestore = inject(Firestore);
+  items$: Observable<any[]>;
+
+  constructor() {
+    const aCollection = collection(this.firestore, 'items')
+    this.items$ = collectionData(aCollection);
+  }
 }
