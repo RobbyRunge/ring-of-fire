b79c48c4ee26026b6714623cc41952bc7d9e6807
RobbyR98 <robbyrunge@yahoo.com>
2025-05-15 20:51:37 +0200

    add Firebase integration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"ring-of-fire-993ea","appId":"1:998685570633:web:8e052ef38e3ab7df197198","storageBucket":"ring-of-fire-993ea.firebasestorage.app","apiKey":"AIzaSyAOnPB1mOVULpLgyE9F0zIzLC4NjELaADE","authDomain":"ring-of-fire-993ea.firebaseapp.com","messagingSenderId":"998685570633"})), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase())]
 };
