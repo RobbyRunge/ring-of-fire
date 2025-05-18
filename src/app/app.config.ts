ad5919e8f8d9ec8b2f4fb8f152ac1fb65b30247e
RobbyR98 <robbyrunge@yahoo.com>
2025-05-18 13:49:39 +0200

    integrate Firebase with Angular application
    - Added @angular/fire package to package.json for Firebase integration.
    - Updated app.config.ts to initialize Firebase with project credentials and set up Firestore.
    - Created .firebaserc for Firebase project configuration.
    - Added firebase.json for hosting configuration.
    - Implemented FirebaseSevicesComponent as a service for future Firebase operations.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimationsAsync(), importProvidersFrom(provideFirebaseApp(() => initializeApp({ 
+    "projectId":"ring-of-fire-6e403",
+    "appId":"1:490845111349:web:f2a6c3422f96f79d705d7c",
+    "storageBucket":"ring-of-fire-6e403.firebasestorage.app",
+    "apiKey":"AIzaSyBVFJ9_PhPm_T2O469L6cNyDFO3ILrXiOc",
+    "authDomain":"ring-of-fire-6e403.firebaseapp.com",
+    "messagingSenderId":"490845111349"})
+  )), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
